f278e768a0e85b37aea831de7121a961d86cbe6c
Francisco Bahamondes <francisco.bahamondes90@gmail.com>
2019-12-23 23:40:34 -0300

    posts.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PostList } from './posts';
 import { UserList } from './users';
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
@@ -6,6 +7,7 @@ import jsonServerProvider from 'ra-data-json-server';
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider}>
+    <Resource name="posts" list={PostList} />
     <Resource name="users" list={UserList} />
   </Admin>
 );
